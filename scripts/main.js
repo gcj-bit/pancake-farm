775410b30c56ef877776554ba269125ae68b1fbb
joey <come.a.mail@qq.com>
2021-04-30 01:52:30 +0800

    更新json数据console输出效果并添加hardhatconfig配置检查

--- scripts/main.js
+++ scripts/main.js
@@ -1,12 +1,14 @@
 const command = require('./utils/command');
-const cache = require('./utils/cache');
 const farm = require('./include/farm');
 const pool = require('./include/pool');
-
-
-
+const hardhatConfig = require('../hardhat.config');
 
 async function main() {
+    //检查hardhat的配置
+    if(hardhatConfig.networks.heco || !hardhatConfig.networks.heco.accounts || !hardhatConfig.networks.heco.accounts[0]) {
+        command.print(hardhatConfig);
+        command.warn("未配置hardhat连接钱包应用于操作的地址账户，无法继续操作！", true); //配置不完整
+    }
 
     // ethers is avaialble in the global scope
     const [deployer] = await ethers.getSigners();
@@ -14,12 +16,11 @@ async function main() {
         "Work with the account:" +
         await deployer.getAddress()
     );
-    command.warn("Account balance:" + (await deployer.getBalance()).toString());
+    command.warn("Account balance:" + (await deployer.getBalance()).toString()); //账户确认，并且必须配置
 
     if(!await command.confirm("是否使用此账户继续操作？")) {
         return;
     }
-
     let index = await command.choices('请选择操作：', [
         '部署Farm：初始化部署Farm功能相关合约',
         '更新Farm：添加LP Token、查询LP Token信息等',

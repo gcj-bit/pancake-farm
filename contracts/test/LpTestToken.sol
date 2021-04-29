7155bf9663210d4e1d3dcdb43697b809965f4328
joey <come.a.mail@qq.com>
2021-04-30 02:07:58 +0800

    move无用合约

--- contracts/test/LpTestToken.sol
+++ contracts/test/LpTestToken.sol
@@ -1,9 +1,7 @@
 //SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.0;
 
-import "hardhat/console.sol";
 import '@openzeppelin/contracts/token/ERC20/ERC20.sol';
-
 
 contract LpTestToken is ERC20 {
     constructor(
